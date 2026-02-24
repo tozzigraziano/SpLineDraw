f45adc68f6532a69eb8b07b66dfe42d4c892c4ff
g.tozzi <g.tozzi@ctfautomazioni.com>
2026-02-24 09:51:00 +0100

    Revert "Enhance FTP upload configuration handling"
    This reverts commit d0b4847a56601a7bcf674d7b5cbca8dd74ed1c32.

--- SpLineDrawMECSPE2026/server.js
+++ SpLineDrawMECSPE2026/server.js
@@ -99,10 +99,7 @@ async function handleApi(req, res) {
                     port: parseInt(ftpPort) || 21,
                     user: ftpUser || 'anonymous',
                     password: ftpPassword || '',
-                    remotePath: ftpRemotePath || '/'
-                };
-
-                const result = await uploadToFtp(ftpConfig, fileName, fileContent);
+                    remotePath: ftpRemotePath || '/'\n                };\n\n                const result = await uploadToFtp(ftpConfig, fileName, fileContent);
 
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(JSON.stringify(result));
